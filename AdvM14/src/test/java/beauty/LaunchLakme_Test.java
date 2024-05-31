6f796fe8c441e01ad7a0c074a326314f5f46909e
Chandana N <Chandana N@TYSS-Chandana>
2024-05-31 07:23:40 +0530

    i have added a script "LanuchLakme_Test" into beauty package.

---
+++ AdvM14/src/test/java/beauty/LaunchLakme_Test.java
@@ -0,0 +1,12 @@
+package beauty;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class LaunchLakme_Test {
+
+	@Test
+	public void lakme() {
+		Reporter.log("lakme application",true);
+	}
+}
