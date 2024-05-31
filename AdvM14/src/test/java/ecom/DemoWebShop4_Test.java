dff85401fb07c47f8d3bb8fcdf4fd6284d97d14c
Chandana N <Chandana N@TYSS-Chandana>
2024-05-31 07:31:53 +0530

    i have added a script "DemoWebShop4_Test" in ecom.

---
+++ AdvM14/src/test/java/ecom/DemoWebShop4_Test.java
@@ -0,0 +1,12 @@
+package ecom;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoWebShop4_Test {
+
+	@Test
+	public void demo4() {
+		Reporter.log("demowebshop",true);
+	}
+}
